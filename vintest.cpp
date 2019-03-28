09cc80d37f874db058627efd4e2e4bceb17846c6
Vvamp <vvampus@gmail.com>
2019-03-28 13:35:10 +0100

    $message

--- vintest.cpp
+++ vintest.cpp
@@ -134,6 +134,7 @@ bool obstacleDetected(){
         }
         if (BP.get_sensor(PORT_2, Ultrasonic2) == 0) {
                 if(Ultrasonic2.cm <= obstacleDetectionDistance){
+                    cout << Ultrasonic2.cm << endl;
                     cout << "works" << endl;
                     return true;
 
