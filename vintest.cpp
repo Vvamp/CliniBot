7f0dbde1f3ca095ea84e65c16e235b90c4e7666e
Vvamp <vvampus@gmail.com>
2019-03-29 10:44:22 +0100

    added a new ctrl + c check function

--- vintest.cpp
+++ vintest.cpp
@@ -13,15 +13,19 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-
-void exit_signal_handler(int signo); // The exit handler definition. Used to catch 'ctrl+c' to terminate execution
-
 BrickPi3 BP; // Define an instance of BrickPi3, called 'BP'
 
 //Sensor definitions
 sensor_light_t      Light3; //RGB Light sensor
 sensor_color_t      Color1; //Infrared sensor
 sensor_ultrasonic_t Ultrasonic2; //Ultrasonic sensor
+
+void eHandler(int s){
+            cout << "Caught ctrl c" << endl;
+            BP.reset_all();
+            exit(0);
+
+}
 
 
 //-- Movement functions
@@ -434,6 +438,15 @@ void debug(){
 // Main execution
 int main()
 {
+    cout << "Setting up ctrl c check..." << endl;
+    struct sigaction sigIntHandler;
+
+    sigIntHandler.sa_handler = eHandler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
     cout << "Setting up sensors..." << endl;
     BP.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers.
 	BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_FULL);
