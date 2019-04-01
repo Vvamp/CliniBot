88340597ff54dcb2ec995a836e41634706595717
Oscar <oscarkromhout@gmail.com>
2019-04-01 13:32:38 +0200

    code opgeschoond. Knop werkt

--- drukSensorTest.cpp
+++ drukSensorTest.cpp
@@ -7,9 +7,6 @@
 using namespace std;
 
 BrickPi3 BP;
-sensor_color_t      Color1;
-sensor_ultrasonic_t Ultrasonic2;
-sensor_light_t      Light3;
 sensor_touch_t      Touch4;
 
 void exit_signal_handler(int signo);
@@ -19,14 +16,7 @@ int main() {
 	signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
 
 	BP.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers.
-	BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_FULL);
-	BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_ULTRASONIC);
-	BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_LIGHT_ON);
 	BP.set_sensor_type(PORT_4, SENSOR_TYPE_TOUCH_NXT);
-	BP.set_motor_limits(PORT_D, 30, 0);
-	BP.set_motor_limits(PORT_B, 30, 0);
-	BP.set_motor_limits(PORT_C, 30, 0);
-
 	while (true) {
 		if (BP.get_sensor(PORT_4, Touch4) == 0) {
 			cout << Touch4.pressed << endl;
