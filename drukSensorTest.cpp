e3324f372dc797d534e28c2182f5e121e91ca02b
Oscar <oscarkromhout@gmail.com>
2019-04-01 13:20:40 +0200

    testscript  voor knopje indrukken toegevoegd

---
+++ drukSensorTest.cpp
@@ -0,0 +1,42 @@
+#include "BrickPi3.h" // for BrickPi3
+#include <iostream>      // for cout
+#include <unistd.h>     // for sleep
+#include <signal.h>     // for catching exit signals
+#include <iomanip>		// for setw and setprecision
+
+using namespace std;
+
+BrickPi3 BP;
+sensor_color_t      Color1;
+sensor_ultrasonic_t Ultrasonic2;
+sensor_light_t      Light3;
+sensor_touch_t      Touch4;
+
+void exit_signal_handler(int signo);
+
+void main() {
+
+	signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
+
+	BP.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers.
+	BP.set_sensor_type(PORT_1, SENSOR_TYPE_NXT_COLOR_FULL);
+	BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_ULTRASONIC);
+	BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_LIGHT_ON);
+	BP.set_sensor_type(PORT_4, SENSOR_TYPE_TOUCH_NXT);
+	BP.set_motor_limits(PORT_D, 30, 0);
+	BP.set_motor_limits(PORT_B, 30, 0);
+	BP.set_motor_limits(PORT_C, 30, 0);
+
+	while (true) {
+		cout << Touch4.pressed << endl;
+		usleep(250000);
+	}
+}
+
+// Signal handler that will be called when Ctrl+C is pressed to stop the program
+void exit_signal_handler(int signo) {
+	if (signo == SIGINT) {
+		BP.reset_all();    // Reset everything so there are no run-away motors
+		exit(-2);
+	}
+}
