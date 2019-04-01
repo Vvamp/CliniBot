8dc1be37fae86d308bd24d7e7022822af00c3482
Vvamp <vvampus@gmail.com>
2019-04-01 13:58:51 +0200

    updates values

--- main.cpp
+++ main.cpp
@@ -723,6 +723,7 @@ void checkGrid(){
     string uinDirection;
     int randomDirectionChooser = 0;
     while(true){
+        srand((unsigned) time(&t));
         randomDirectionChooser = rand() % 3 + 1;
         cout << randomDirectionChooser << endl;
         switch(randomDirectionChooser){
