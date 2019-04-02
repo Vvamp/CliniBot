797ca853d2ee59f59574b7fbe22916c259e988d8
Oscar <oscarkromhout@gmail.com>
2019-04-02 14:47:39 +0200

    x

--- main.cpp
+++ main.cpp
@@ -29,10 +29,14 @@ int blackHigh = 0;
 int blackLow = 1000000;
 int whiteHigh = 0;
 int whiteLow = 10000000;
+int RGBBlackLow = 1000000;
+int RGBBlackHigh = 0;
+int RGBWhiteLow = 1000000;
+int RGBWhiteHigh = 0;
 
 void Calibration() {
 	int time = 4000;
-	int stepper = 0;
+	int average;
 	char input;
 	while (true) {
 		if (blackHigh == 0) {
@@ -47,15 +51,24 @@ void Calibration() {
 						if (Light3.reflected < blackLow) {
 							blackLow = Light3.reflected;
 						}
-						stepper++;
-						usleep(1000);
 					}
+					if (BP.get_sensor(PORT_1, Color1) == 0) {
+						average = (Color1.reflected_blue + Color1.reflected_green + Color1.reflected_red) / 3;
+						if (average > RGBBlackHigh) {
+							RGBBlackHigh = average;
+						}
+						if (average < RGBBlackLow) {
+							RGBBlackLow = average;
+						}
+					}
+					usleep(1000);
 				}
 			}
-			stepper = 0;
 			input = ' ';
 			cout << "black high value is: " << blackHigh << endl;
 			cout << "black low value is: " << blackLow << endl;
+			cout << "RGB black high value is: " << RGBBlackHigh << endl;
+			cout << "RGB black low value is: " << RGBBlackLow << endl;
 		}
 		if (whiteHigh == 0) {
 			cout << "place the robot on white and press s + enter to start" << endl;
@@ -69,17 +82,26 @@ void Calibration() {
 						if (Light3.reflected < whiteLow) {
 							whiteLow = Light3.reflected;
 						}
-						stepper++;
-						usleep(1000);
 					}
+					if (BP.get_sensor(PORT_1, Color1) == 0) {
+						average = (Color1.reflected_blue + Color1.reflected_green + Color1.reflected_red) / 3;
+						if (average > RGBBlackHigh) {
+							RGBBlackHigh = average;
+						}
+						if (average < RGBBlackLow) {
+							RGBBlackLow = average;
+						}
+					}
+					usleep(1000);
 				}
 			}
-			stepper = 0;
 			input = ' ';
 			cout << "white high value is: " << whiteHigh << endl;
 			cout << "white low value is: " << whiteLow << endl;
+			cout << "RGB white high value is: " << RGBWhiteHigh << endl;
+			cout << "RGB white low value is: " << RGBWhiteLow << endl;
 		}
-		if (blackLow > whiteHigh) {
+		if (blackLow > whiteHigh && RGBBlackLow > RGBWhiteHigh) {
 			break;
 		}
 		else {
@@ -88,6 +110,10 @@ void Calibration() {
 			blackHigh = 0;
 			whiteLow = 1000000;
 			whiteHigh = 0;
+			RGBBlackLow = 1000000;
+			RGBBlackHigh = 0;
+			RGBWhiteLow = 1000000;
+			RGBWhiteHigh = 0;
 			input = ' ';
 		}
 	}
