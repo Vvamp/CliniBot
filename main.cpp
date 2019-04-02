66797f945d863958012c6e32152048c1f2616aaa
Vvamp <vvampus@gmail.com>
2019-04-02 14:29:06 +0200

    changed static values to variables

--- main.cpp
+++ main.cpp
@@ -224,133 +224,6 @@ void turnRight(){
     BP.set_motor_position_relative(PORT_C, speed);
 }
 
-void obstacleAvoidenceFwd() {
-	cout << "obstacle avoidence, going forward..." << endl;
-	int object = false;
-	int timer = 0;
-	BP.set_motor_position_relative(PORT_D, -90);
-	moveFwd(4000000);
-	moveStop();
-	while (timer < 2000) {
-		if (BP.get_sensor(PORT_2, Ultrasonic2) == 0) {
-			timer++;
-			usleep(1000);
-			if (Ultrasonic2.cm < 30) {
-				object = true;
-			}
-			else {
-				object = false;
-			}
-		}
-		else {
-			cout << "can't find ultrasonic sensor" << endl;
-		}
-
-	}
-	BP.set_motor_position_relative(PORT_D, 90);
-	usleep(1000000);
-	if (object == true) {
-		obstacleAvoidenceFwd();
-	}
-	else
-	{
-		return;
-	}
-}
-
-void obstacleAvoidenceLeft() {
-	cout << "obstacle avoidence, going left..." << endl;
-	BP.set_motor_position_relative(PORT_B, 290);
-	BP.set_motor_position_relative(PORT_C, -290);
-	usleep(1500000);
-	return;
-}
-void obstacleAvoidenceRight() {
-	cout << "obstacle avoidence, going left..." << endl;
-	BP.set_motor_position_relative(PORT_B, -270);
-	BP.set_motor_position_relative(PORT_C, 270);
-	usleep(1500000);
-	return;
-}
-
-void obstacleAvoidenceEnding() {
-	cout << "obstacle avoidence, searching for the line" << endl;
-	while (true) {
-		if (BP.get_sensor(PORT_3, Light3) == 0) {
-			if (Light3.reflected > 1800 && Light3.reflected < 2100) {
-				moveFwd(100000);
-			}
-			else
-			{
-				moveFwd(500000);
-				moveLeft(250000);
-				moveStop();
-				return;
-			}
-
-		}
-	}
-}
-
-void avoidObstacle() {
-	cout << "starting obstacle detection..." << endl;
-	obstacleAvoidenceLeft();
-	obstacleAvoidenceFwd();
-	obstacleAvoidenceRight();
-	obstacleAvoidenceFwd();
-	obstacleAvoidenceRight();
-	obstacleAvoidenceEnding();
-}
-
-void driveByLine() {
-
-	if (BP.get_voltage_battery() >= 9) {
-		while (true) {
-			if (BP.get_sensor(PORT_2, Ultrasonic2) == 0) {
-				if (BP.get_sensor(PORT_3, Light3) == 0) {
-					cout << "searching line..." << endl;
-
-					if (Ultrasonic2.cm > 10) {
-						cout << Light3.reflected << endl;
-						if (Light3.reflected > 1800 && Light3.reflected < 2000) {
-							cout << "wit" << endl;
-							moveLeft(100000);
-							//als ie het wit in gaat
-						}
-						else if (Light3.reflected >= 2000 && Light3.reflected <= 2200) {
-							cout << "half" << endl;
-							moveFwd(100000);
-							//rechtdoor
-						}
-						else if (Light3.reflected > 2200) {
-							moveRight(100000);
-							cout << "zwart" << endl;
-							//als ie het zwart in gaat
-						}
-						cout << "\033[2J\033[1;1H";
-					}
-					else {
-						avoidObstacle();
-					}
-				}
-				else
-				{
-					cout << "Can't read ultra-red sensor..." << endl;
-				}
-			}
-			else {
-				cout << "Can't read ultrasonic sensor..." << endl;
-			}
-		}
-		usleep(100000);
-	}
-	else {
-		cout << "Battery voltage is: " << BP.get_voltage_battery() << ". This is to low to continue..." << endl;
-	}
-	cout << "Robot stopped..." << endl;
-
-}
-
 
 //-Eye functions
 const int lookAngle = 105;
@@ -411,7 +284,7 @@ bool isCrossing(){
     /*    if(enableDebug){
         cout << "ir: " << measurement << endl;
     }*/
-        if(measurement >= 2200){
+        if(measurement >= blackLow){
             s2 = true;
         }
     }
@@ -462,7 +335,7 @@ bool lineDetected(){
         if(enableDebug){
         cout << endl << "measured RGB: " << measurement << " borders: 2000 <= x < 2700" << endl;
         }
-        if(measurement >=2000 && measurement < 2700){
+        if(measurement >=whiteHigh && measurement < blackHigh){
             s1 = true;
         }else{
             s1 = false;
@@ -577,7 +450,7 @@ void checkGrid(){
 				moveStop();
 				while (true) {
 					if (BP.get_sensor(PORT_3, Light3) == 0) {
-						if (Light3.reflected <= 2300) {
+						if (Light3.reflected <= blackLow) {
 							moveLeft(100000);
 							stepsLeft += 100000;
 						}
@@ -650,7 +523,7 @@ void checkGrid(){
 					moveStop();
 					while (true) {
 						if (BP.get_sensor(PORT_3, Light3) == 0) {
-							if (Light3.reflected <= 2200) {
+							if (Light3.reflected <= blackLow) {
 								moveRight(100000);
 								stepsRight += 100000;
 							}
@@ -849,18 +722,18 @@ void controlGrid(){
                 }
             }
 			if (BP.get_sensor(PORT_3, Light3) == 0) {
-				if (Light3.reflected >= 2200 && Light3.reflected <= 2300) {
+				if (Light3.reflected >= whiteHigh && Light3.reflected <= blackLow) { //2200 - 2300
                     if(isSearchingAfterCrossing){
                         isSearchingAfterCrossing = false;
                     }
                     moveBot(Light3.reflected, 50, 50);
 					//rechtdoor
 				}
-				else if (Light3.reflected > 1850 && Light3.reflected < 2200) {
+				else if (Light3.reflected > whiteLow && Light3.reflected < whiteHigh) { //1850 - 2200
                     moveBot(Light3.reflected, 5, 50);
 					//als ie het wit in gaat
 				}
-				else if (Light3.reflected > 2300) {
+				else if (Light3.reflected > blackLow) { // > 2300
                     if(isCrossing()){
                         if(!isSearchingAfterCrossing){
                         moveStop();
