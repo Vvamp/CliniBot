61cc01aa28f97e47e44a6cf20004583a2d4ad4f6
Vvamp <vvampus@gmail.com>
2019-04-11 10:10:18 +0200

    merged dans script with main file -- last commit

--- main.cpp
+++ main.cpp
@@ -25,6 +25,12 @@ sensor_color_t      Color1; //Infrared sensor
 sensor_ultrasonic_t Ultrasonic2; //Ultrasonic sensor
 sensor_touch_t      Touch4;		// Touch sensor
 
+// Dans kleur variabelen
+int Rood;
+int Groen;
+int Blauw;
+
+
 //Bluetooth definition
 BluetoothServerSocket serversock(2, 1);  //2 is het channel-number
 BluetoothSocket* clientsock;
@@ -1047,8 +1053,225 @@ void controlGrid(){
 
 
 
+// Dans scripts
+void Dansje(){
 
+    BP.set_motor_limits(PORT_B, 90, 0);
+    BP.set_motor_limits(PORT_C, 90, 0);
 
+    if(Rood > Groen && Rood > Blauw){
+
+        cout << "=> Rode dans !!" << endl;
+
+        lookLeft();
+        usleep(500000);
+        lookRight();
+        usleep(500000);
+        lookRight();
+        usleep(500000);
+        lookLeft();
+        usleep(500000);
+        lookLeft();
+        usleep(500000);
+
+        BP.set_motor_position_relative(PORT_B, 420);
+        BP.set_motor_position_relative(PORT_C, -420);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, 420);
+        BP.set_motor_position_relative(PORT_C, -420);
+        sleep(1);
+
+        BP.set_motor_position_relative(PORT_B, 0);
+        BP.set_motor_position_relative(PORT_C, 0);
+
+        lookRight();
+        usleep(500000);
+        lookRight();
+        usleep(500000);
+        lookLeft();
+        usleep(500000);
+        lookRight();
+        usleep(500000);
+
+        BP.set_motor_position_relative(PORT_B, -420);
+        BP.set_motor_position_relative(PORT_C, 420);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, -420);
+        BP.set_motor_position_relative(PORT_C, 420);
+        sleep(1);
+        BP.set_motor_position_relative(PORT_B, 0);
+        BP.set_motor_position_relative(PORT_C, 0);
+
+        lookLeft();
+        usleep(500000);
+        lookLeft();
+        usleep(500000);
+
+        BP.set_motor_position_relative(PORT_B, -420);
+        BP.set_motor_position_relative(PORT_C, 420);
+        sleep(1);
+        BP.set_motor_position_relative(PORT_B, 0);
+        BP.set_motor_position_relative(PORT_C, 0);
+
+        lookRight();
+        usleep(500000);
+        lookRight();
+        usleep(500000);
+
+        BP.set_motor_position_relative(PORT_B, 420);
+        BP.set_motor_position_relative(PORT_C, -420);
+        sleep(1);
+        BP.set_motor_position_relative(PORT_B, 0);
+        BP.set_motor_position_relative(PORT_C, 0);
+
+        lookLeft();
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, -410);
+        BP.set_motor_position_relative(PORT_C, -410);
+        usleep(500000);
+        lookLeft();
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, 410);
+        BP.set_motor_position_relative(PORT_C, 410);
+        lookRight();
+        usleep(500000);
+        lookRight();
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, -1680);
+        BP.set_motor_position_relative(PORT_C, 1680);
+        usleep(750000);
+        lookLeft();
+        sleep(3);
+
+        }
+    else if(Groen > Rood && Groen > Blauw){
+
+        cout << "=> Groene Dans !!" << endl;
+
+        BP.set_motor_position_relative(PORT_B, 360);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_C, -360);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, -360);
+        BP.set_motor_position_relative(PORT_C, -360);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, 210);
+        BP.set_motor_position_relative(PORT_C, -210);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, -210);
+        BP.set_motor_position_relative(PORT_C, 210);
+        sleep(1);
+
+        BP.set_motor_position_relative(PORT_C, 0);
+        BP.set_motor_position_relative(PORT_C, 0);
+
+        lookLeft();
+        usleep(500000);
+
+        BP.set_motor_position_relative(PORT_C, 360);
+        BP.set_motor_position_relative(PORT_C, 360);
+        sleep(1);
+
+        BP.set_motor_position_relative(PORT_C, 0);
+        BP.set_motor_position_relative(PORT_C, 0);
+
+        lookRight();
+        usleep(500000);
+        lookRight();
+        usleep(500000);
+
+        BP.set_motor_position_relative(PORT_B, 360);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_C, -360);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, 360);
+        BP.set_motor_position_relative(PORT_C, 360);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, -210);
+        BP.set_motor_position_relative(PORT_C, 210);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, 210);
+        BP.set_motor_position_relative(PORT_C, -210);
+        sleep(1);
+
+        BP.set_motor_position_relative(PORT_B, 0);
+        BP.set_motor_position_relative(PORT_C, 0);
+
+        lookLeft();
+        usleep(500000);
+
+        BP.set_motor_position_relative(PORT_B, -1680);
+        BP.set_motor_position_relative(PORT_C, 1680);
+        sleep(3);
+
+    }
+
+    else if(Blauw > Rood && Blauw > Groen){
+
+        cout << "=> Blauwe dans !!" << endl;
+
+        BP.set_motor_position_relative(PORT_B, -1680);
+        BP.set_motor_position_relative(PORT_C, 1680);
+        usleep(1000000);
+        BP.set_motor_position_relative(PORT_B, 1680);
+        BP.set_motor_position_relative(PORT_C, -1680);
+        usleep(1000000);
+        BP.set_motor_position_relative(PORT_B, 210);
+        BP.set_motor_position_relative(PORT_C, -210);
+        lookLeft();
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, -210);
+        BP.set_motor_position_relative(PORT_C, 210);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, 210);
+        BP.set_motor_position_relative(PORT_C, -210);
+        lookRight();
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, -210);
+        BP.set_motor_position_relative(PORT_C, 210);
+        usleep(500000);
+        BP.set_motor_position_relative(PORT_B, 210);
+        BP.set_motor_position_relative(PORT_C, -210);
+        lookRight();
+        usleep(750000);
+        BP.set_motor_position_relative(PORT_B, -840);
+        BP.set_motor_position_relative(PORT_C, 840);
+        usleep(750000);
+        lookLeft();
+        sleep(3);
+    }
+    else{
+        cout << "Ongeldige waarde !!!" << endl;
+    }
+
+}
+
+void MeetKleuren(){
+
+while(true){
+
+    sleep(1);
+
+    if(BP.get_sensor(PORT_1, Color1) == 0){
+
+    Rood = Color1.reflected_red;
+    Groen = Color1.reflected_green;
+    Blauw = Color1.reflected_blue;
+
+    cout << endl;
+    cout << "Kleurwaarde voor Rood = "<< Rood << endl;
+    cout << "Kleurwaarde voor Groen = " << Groen << endl;
+    cout << "Kleurwaarde voor Blauw = " << Blauw << endl;
+
+    Dansje();
+
+    }
+
+    else{
+        cout << "Sensor werkt niet" << endl;
+        }
+    }
+}
 
 int main(){
 // Initialize the ctrl + c catch
@@ -1114,11 +1337,11 @@ if(Keuze == "1"){
 	cin >> input;
 	controlGrid();
 }
-else if(Keuze == "3"){
+else if(Keuze == "4"){
     cout << "...Exiting Keuzemenu..." << endl;
     break;
-}else if(Keuze == "4"){
-	shakeHead();
+}else if(Keuze == "3"){
+	MeetKleuren();
 	break;
 }else{
     cout << "Geen geldige keuze! Probeer opnieuw..." << endl;
